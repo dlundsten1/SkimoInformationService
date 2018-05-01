f7f7d18d69b9ea16210467f9feb1c51e0e36a9ad
Andreas Lundsten <andreas.lundsten@ffcg.se>
2018-05-01 12:23:10 +0200

    Swagger och lite annat.

--- sims-api/src/main/java/se/lundsten/model/rest/IndividualRestPath.java
+++ sims-api/src/main/java/se/lundsten/model/rest/IndividualRestPath.java
@@ -3,5 +3,6 @@ package se.lundsten.model.rest;
 
 public interface IndividualRestPath {
   String INDIVIDUAL_PATH = "/individual";
-  String FIND_BY_ID = "/id";
+  String FIND_BY_ID = "/id/{individual-id}";
+  String FIND_ALL = "/all";
 }
