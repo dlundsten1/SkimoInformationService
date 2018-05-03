d5c66fb0310c64326550438d6eb9b83187f4889b
DavidLundsten <dlundsten@gmail.com>
2018-05-03 10:18:28 +0200

    Databaskoppling igen

--- sims-app/src/main/java/se/lundsten/Repository/MongoConnection.java
+++ sims-app/src/main/java/se/lundsten/Repository/MongoConnection.java
@@ -1,13 +1,16 @@
 package se.lundsten.Repository;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public final class MongoConnection {
-   private MongoClient mongoClient = new MongoClient(DbPath.DB_HOST_CONNECTION, DbPath.DB_PORT);
-   private DB database = mongoClient.getDB(DbPath.DB_NAME);
-   private DBCollection collection = database.getCollection(DbPath.DB_INDIVIDUALS_COLLECTION);
+
+    public MongoDatabase initConnection () {
+    MongoClient mongoClient = MongoClients.create();
+    MongoDatabase database = mongoClient.getDatabase(DbPath.DB_NAME);
+    return database;
+    }
+
 }
