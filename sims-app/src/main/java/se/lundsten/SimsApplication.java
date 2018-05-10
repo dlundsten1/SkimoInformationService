80c1622b89e8b4971d94e216b2bed6b9fa7ec2e5
Andreas Lundsten <andreas.lundsten@ffcg.se>
2018-05-10 20:06:52 +0200

    Plockade bort ProperySource.

--- sims-app/src/main/java/se/lundsten/SimsApplication.java
+++ sims-app/src/main/java/se/lundsten/SimsApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.${spring.profiles.active:dev}.properties")
 public class SimsApplication extends SpringBootServletInitializer {
 
   @Override
